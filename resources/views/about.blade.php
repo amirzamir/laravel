fa58b7b09aeab6c33e6b03ffe043a30feb398285
amirzamir <amir@live.com.my>
2021-08-10 17:47:21 +0800

    commit view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Coba | about</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3>Amir Zamir</h3>
+    <p>amir@live.com.my</p>
+    <img src="img/amir.jpg" alt="AmirZamir">
+</body>
+</html>
